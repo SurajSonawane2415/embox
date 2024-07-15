b677fd7e21663f319ae1f21428753a53e7268dfe
SurajSonawane2415 <surajsonawane0215@gmail.com>
2024-07-15 07:59:01 +0530

    drivers: Refactored timer from riscv_clint.c to riscv_clk.c and integrated SiFive CLINT

--- src/drivers/clock/riscv_clk/riscv_clk.c
+++ src/drivers/clock/riscv_clk/riscv_clk.c
@@ -1,10 +1,12 @@
 /**
  * @file
  *
- * @brief RISC-V build-in timer
+ * @brief RISC-V built-in timer with RISC-V and SiFive CLINT integration.
  *
  * @date 12.12.2019
- * @author Anastasia Nizharadze
+ * @date 14.07.2024 (CLINT timer integration)
+ * @authored by Anastasia Nizharadze
+ * @authored by Suraj Ravindra Sonawane (CLINT timer integration)
  */
 
 #include <errno.h>
@@ -26,44 +28,114 @@
 #define MTIME         OPTION_GET(NUMBER, base_mtime)
 #define MTIMECMP      OPTION_GET(NUMBER, base_mtimecmp)
 
+#ifdef SIFIVE_CLINT
+#define MTIMECMP_HART(hart)  (MTIMECMP + (hart) * 8)
+#define MAX_HART        5
+#else
+#define MTIMECMP_HART(hart)  MTIMECMP
+#endif
+
 #define OPENSBI_TIMER 0x54494D45
 
-static int clock_handler(unsigned int irq_nr, void *dev_id) {
-#if SMODE
-	register uintptr_t a7 asm("a7") = (uintptr_t)(OPENSBI_TIMER);
-	register uintptr_t a6 asm("a6") = (uintptr_t)(0);
-	register uintptr_t a0 asm("a0") = 0;
-	asm volatile("rdtime a0");
-	a0 = a0 + COUNT_OFFSET;
-	(void)a7;
-	(void)a6;
-	asm volatile("ecall");
+/**
+ * Initializes the CLINT timer module.
+ *
+ * This function initializes the CLINT timer by setting MTIMECMP to its maximum value and initializing MTIME to 0.
+ *
+ * @return 0 on success.
+ */
+static int clint_timer_init(void) {
+#ifdef SIFIVE_CLINT
+    int hart;
+    for (hart = 0; hart < MAX_HART; hart++) {
+        REG64_STORE(MTIMECMP_HART(hart), 0xFFFFFFFFFFFFFFFF); // Set MTIMECMP to max value for each hart
+    }
 #else
-	REG64_STORE(MTIMECMP, REG64_LOAD(MTIME) + COUNT_OFFSET);
+    REG64_STORE(MTIMECMP_HART(0), 0xFFFFFFFFFFFFFFFF); // Set MTIMECMP to max value
 #endif
-
-	clock_tick_handler(dev_id);
-
-	return IRQ_HANDLED;
+    REG64_STORE(MTIME, 0); // Initialize MTIME to 0
+    return 0;
 }
 
-static int riscv_clock_setup(struct clock_source *cs) {
+/**
+ * Sets the MTIMECMP register value for the specific hart.
+ *
+ * This function sets the MTIMECMP register to the provided 64-bit value for the specified hart.
+ *
+ * @param hart The hart id.
+ * @param value The value to set for MTIMECMP.
+ */
+static void clint_set_mtimecmp(int hart, uint64_t value) {
+    REG64_STORE(MTIMECMP_HART(hart), value); // Write 'value' to MTIMECMP for the specified hart
+}
+
+/**
+ * Retrieves the current value of MTIME.
+ *
+ * This function reads and returns the current value of MTIME.
+ *
+ * @return The current value of MTIME.
+ */
+static uint64_t clint_get_mtime(void) {
+    return REG64_LOAD(MTIME); // Read and return the value at MTIME
+}
+
+static int clock_handler(unsigned int irq_nr, void *dev_id
+#ifdef SIFIVE_CLINT
+    , int hart
+#endif
+) {
 #if SMODE
-	register uintptr_t a7 asm("a7") = (uintptr_t)(OPENSBI_TIMER);
-	register uintptr_t a6 asm("a6") = (uintptr_t)(0);
-	register uintptr_t a0 asm("a0") = 0;
-	asm volatile("rdtime a0");
-	a0 = a0 + COUNT_OFFSET;
-	(void)a7;
-	(void)a6;
-	asm volatile("ecall");
+    register uintptr_t a7 asm("a7") = (uintptr_t)(OPENSBI_TIMER);
+    register uintptr_t a6 asm("a6") = (uintptr_t)(0);
+    register uintptr_t a0 asm("a0") = 0;
+    asm volatile("rdtime a0");
+    a0 = a0 + COUNT_OFFSET;
+    (void)a7;
+    (void)a6;
+    asm volatile("ecall");
 #else
-	REG64_STORE(MTIMECMP, REG64_LOAD(MTIME) + COUNT_OFFSET);
+#ifdef SIFIVE_CLINT
+    if (hart < MAX_HART) {
+        clint_set_mtimecmp(hart, clint_get_mtime() + COUNT_OFFSET);
+    }
+#else
+    clint_set_mtimecmp(0, clint_get_mtime() + COUNT_OFFSET);
+#endif
 #endif
 
-	enable_timer_interrupts();
+    clock_tick_handler(dev_id);
 
-	return ENOERR;
+    return IRQ_HANDLED;
+}
+
+static int riscv_clock_setup(struct clock_source *cs
+#ifdef SIFIVE_CLINT
+    , int hart
+#endif
+) {
+#if SMODE
+    register uintptr_t a7 asm("a7") = (uintptr_t)(OPENSBI_TIMER);
+    register uintptr_t a6 asm("a6") = (uintptr_t)(0);
+    register uintptr_t a0 asm("a0") = 0;
+    asm volatile("rdtime a0");
+    a0 = a0 + COUNT_OFFSET;
+    (void)a7;
+    (void)a6;
+    asm volatile("ecall");
+#else
+#ifdef SIFIVE_CLINT
+    if (hart < MAX_HART) {
+        clint_set_mtimecmp(hart, clint_get_mtime() + COUNT_OFFSET);
+    }
+#else
+    clint_set_mtimecmp(0, clint_get_mtime() + COUNT_OFFSET);
+#endif
+#endif
+
+    enable_timer_interrupts();
+
+    return ENOERR;
 }
 
 static struct time_event_device riscv_event_device = {
--- src/drivers/interrupt/riscv_clint/riscv_clint.c
+++ src/drivers/interrupt/riscv_clint/riscv_clint.c
@@ -1,72 +1,69 @@
 /**
  * @file
  *
- * @brief Implementation of the RISC-V Core Local Interruptor (CLINT) for interrupt control and timer management.
- *
+ * @brief Implementation of the RISC-V Core Local Interruptor (CLINT) for interrupt control, including support for SiFive multi-hart architectures.
+ * 
  * @date 05.07.2024
  * @authored by Suraj Ravindra Sonawane
  */
 
 #include <stdint.h>
 #include <drivers/irqctrl.h>
+#include <asm/regs.h>
 #include <hal/reg.h>
 
 #define CLINT_ADDR      OPTION_GET(NUMBER, base_addr)
 #define MSIP_OFFSET     OPTION_GET(NUMBER, msip_offset)
-#define MTIMECMP_OFFSET OPTION_GET(NUMBER, mtimecmp_offset)
-#define MTIME_OFFSET    OPTION_GET(NUMBER, mtime_offset)
 
 #define MSIP_ADDR       (CLINT_ADDR + MSIP_OFFSET)
-#define MTIMECMP_ADDR   (CLINT_ADDR + MTIMECMP_OFFSET)
-#define MTIME_ADDR      (CLINT_ADDR + MTIME_OFFSET)
+
+#ifdef SIFIVE_CLINT
+#define MAX_HART        5
+#endif
 
 /**
  * Initializes the CLINT.
  *
- * This function initializes the CLINT by clearing the MSIP (Software Interrupt) and setting MTIMECMP
- * to its maximum value (0xFFFFFFFFFFFFFFFF).
+ * This function initializes the CLINT by clearing the MSIP (Software Interrupt).
  *
  * @return 0 on success.
  */
 static int clint_init(void) {
-    // Initial configuration: clear MSIP and set MTIMECMP to max value
-    REG32_STORE(MSIP_ADDR, 0);                      // Clear MSIP by writing 0 to its address
-    REG64_STORE(MTIMECMP_ADDR, 0xFFFFFFFFFFFFFFFF); // Set MTIMECMP to max value
-    REG64_STORE(MTIME_ADDR, 0);                     // Initialize MTIME to 0
+#ifdef SIFIVE_CLINT
+    // Initial configuration: clear MSIP for all harts
+    for (int hart_id = 0; hart_id < MAX_HART; hart_id++) {
+        REG32_STORE(MSIP_ADDR + (hart_id * 4), 0);
+    }
+#else
+    // Initial configuration: clear MSIP for a single hart
+    REG32_STORE(MSIP_ADDR, 0);
+#endif
+    enable_software_interrupts();
     return 0;
 }
 
 /**
  * Configures the Software Interrupt (MSIP).
  *
- * This function configures the MSIP by writing a specific value (0 or 1) to its address.
- *
  * @param value The value (0 or 1) to set for MSIP.
+ * @param hart_id The ID of the hart to configure MSIP for (only used if SIFIVE_CLINT is defined).
  */
-void clint_configure_msip(uint8_t value) {
+void clint_configure_msip(uint8_t value
+#ifdef SIFIVE_CLINT
+    , int hart_id
+#endif
+) {
+#ifdef SIFIVE_CLINT
+    if (hart_id < MAX_HART) {
+        REG32_STORE(MSIP_ADDR + (hart_id * 4), value & 1); // Write the LSB of 'value' to the MSIP register of the specified hart
+    }
+#else
     REG32_STORE(MSIP_ADDR, value & 1); // Write the least significant bit of 'value' to MSIP_ADDR
+#endif
 }
 
-/**
- * Sets the MTIMECMP register value.
- *
- * This function sets the MTIMECMP register to the provided 64-bit value.
- *
- * @param value The value to set for MTIMECMP.
- */
-void clint_set_mtimecmp(uint64_t value) {
-    REG64_STORE(MTIMECMP_ADDR, value); // Write 'value' to MTIMECMP_ADDR
-}
-
-/**
- * Retrieves the current value of MTIME.
- *
- * This function reads and returns the current value of MTIME.
- *
- * @return The current value of MTIME.
- */
-uint64_t clint_get_mtime(void) {
-    return REG64_LOAD(MTIME_ADDR); // Read and return the value at MTIME_ADDR
-}
-
+#ifdef SIFIVE_CLINT
+IRQCTRL_DEF(sifive_clint, clint_init);
+#else
 IRQCTRL_DEF(riscv_clint, clint_init);
+#endif
